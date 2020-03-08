579eed459f14b1e158bb7f7a56fbf46b9c2d6d59
Christopher Jones <chris.jones@wessexlabels.co.uk>
2020-03-08 09:14:45 +0000

    Adding generic host / DI support

--- src/PhotoImport.App/App.xaml.cs
+++ src/PhotoImport.App/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace PhotoImport.App
 {
@@ -13,5 +15,33 @@ namespace PhotoImport.App
    /// </summary>
    public partial class App : Application
    {
+      private readonly IHost _host;
+
+      public App()
+      {
+         _host = new HostBuilder()
+            .ConfigureServices((context, services) =>
+            {
+               services.AddSingleton<MainWindow>();
+            })
+            .Build();
+      }
+
+      private void App_OnStartup(object sender, StartupEventArgs e)
+      {
+         _host.StartAsync().Wait();
+
+         MainWindow = _host.Services.GetRequiredService<MainWindow>();
+         MainWindow.Show();
+      }
+
+      private void App_OnExit(object sender, ExitEventArgs e)
+      {
+         using (_host)
+         {
+            _host.StopAsync().Wait();
+         }
+         
+      }
    }
 }
